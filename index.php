72551b73e6f02018363c45fcbf47bcd7c03be61e
shorttime4146 <shorttimememoryloss4146@gmail.com>
2025-12-03 23:32:38 -0800

    adminplan

---
+++ index.php
@@ -0,0 +1,18 @@
+<!--begin::Header-->
+  <?php require 'header.php'; ?>
+<!--end::Header-->
+
+<!--begin::Menu-->
+  <?php 
+    require 'header_menu.php';
+    require 'side_menu.php'; 
+  ?>
+<!--end::Menu-->
+
+<!--begin::App Main-->
+  <?php require 'main_body.php'; ?>
+<!--end::App Main-->
+
+<!--begin::Footer-->
+  <?php require 'footer.php'; ?>
+<!--end::Footer-->
